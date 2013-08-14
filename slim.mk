0f351d44b33400158514560f269fdab33a3179ed
Josue Rivera <prbassplayer@gmail.com>
2013-08-14 06:59:57 +0200

    Galaxysmtd: add target width and height for boot animation
    Change-Id: Iec98a708ef44350ba2a8c73649a1afb4acfadae9
    Signed-off-by: Josue Rivera <prbassplayer@gmail.com>

--- slim.mk
+++ slim.mk
@@ -4,6 +4,10 @@ $(call inherit-product, vendor/slim/config/gsm.mk)
 # Release name
 PRODUCT_RELEASE_NAME := galaxysmtd
 
+# Boot animation
+TARGET_SCREEN_HEIGHT := 800
+TARGET_SCREEN_WIDTH := 480
+
 # Inherit some common slim stuff.
 $(call inherit-product, vendor/slim/config/common_full_phone.mk)
 
